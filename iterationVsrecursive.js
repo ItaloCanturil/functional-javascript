b72d342c3262b59c6e9bfeeda63e54dae35d8383
Italo Canturil <italo.cantur@gmail.com>
2022-05-17 21:09:05 -0300

    exercise

--- iterationVsrecursive.js
+++ iterationVsrecursive.js
@@ -1,12 +1,16 @@
 function iterativeFibonacci(n) {
   // TODO your code goes here
-  if(n === 0 || n === 1) return n;
-  let num = 0;
-  for(let i = 0; i < n; i++) {
-    num = (n-1) + (n-2)
-    console.log(n - 1)
+  const fib = [0, 1]
+  for(let i = 2; i <= 20; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
   }
-  return num
+  return fib[n]
 }
 
-console.log(iterativeFibonacci(6));
+console.log(iterativeFibonacci(20));
+
+function recursiveFibonacci(n) {
+  // TODO your code goes here
+  if (n === 0 || n === 1) return n;
+  return recursiveFibonacci(n-2) + recursiveFibonacci(n-1);
+}
